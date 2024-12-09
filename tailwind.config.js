8a5c6f09983e63b20f0f8b1ea0609dc3ade4eb9e
yefferson2003 <ysantmaria@unicesar.edu.co>
2024-12-08 20:39:21 -0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-const { addDynamicIconSelectors } = require('@iconify/tailwind')
+// const { addDynamicIconSelectors } = require('@iconify/tailwind')
 
 export default {
     darkMode: ["class"],
     content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			principal: '#ff0000',
-  			fondo: '#e0e0e0',
-  			grisClaro: '#f0f0f0'
-  		},
-  		screens: {
-  			tablet: '900px',
-  			tablet2: '600px'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [addDynamicIconSelectors(), require("tailwindcss-animate")]
+"./src/**/*.{js,ts,jsx,tsx}",
+],
+theme: {
+extend: {
+	colors: {
+		principal: '#ff0000',
+		fondo: '#e0e0e0',
+		grisClaro: '#f0f0f0'
+	},
+	screens: {
+		tablet: '900px',
+		tablet2: '600px'
+	},
+	borderRadius: {
+		lg: 'var(--radius)',
+		md: 'calc(var(--radius) - 2px)',
+		sm: 'calc(var(--radius) - 4px)'
+	}
+}
+},
+// plugins: [addDynamicIconSelectors(), require("tailwindcss-animate")]
 }
